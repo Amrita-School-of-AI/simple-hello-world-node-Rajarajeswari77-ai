0284c5b7320b83c8254cc47b894fc775a4a5403e
Raji <your-github-email@example.com>
2026-01-03 23:20:51 +0530

    Add ROS2 hello world! node

--- src/hello_node.cpp
+++ src/hello_node.cpp
@@ -1,13 +1,32 @@
 #include "rclcpp/rclcpp.hpp"
+#include <chrono>
+
+using namespace std::chrono_literals;
+
+class HelloNode : public rclcpp::Node
+{
+public:
+    HelloNode() : Node("hello_world_node")
+    {
+        timer_ = this->create_wall_timer(
+            1000ms,
+            std::bind(&HelloNode::timer_callback, this)
+        );
+    }
+
+private:
+    void timer_callback()
+    {
+        RCLCPP_INFO(this->get_logger(), "Hello, World!");
+    }
+
+    rclcpp::TimerBase::SharedPtr timer_;
+};
 
 int main(int argc, char ** argv)
 {
     rclcpp::init(argc, argv);
-
-    auto node = std::make_shared<rclcpp::Node>("hello_node");
-
-    RCLCPP_INFO(node->get_logger(), "Hello World");
-
+    rclcpp::spin(std::make_shared<HelloNode>());
     rclcpp::shutdown();
     return 0;
 }
